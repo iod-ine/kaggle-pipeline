1e8fe0162630c1b93d9b78e7c994e2ae01d62eee
Ivan Dubrovin <io.dubrovin@icloud.com>
2023-01-14 16:09:05 +0300

    Create READMEs for src and its submodules

---
+++ src/README.md
@@ -0,0 +1,5 @@
+To run executable scripts, from the project root do
+
+``` shell
+python -m src.submodule.script
+```
--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,1 @@
-"""This makes src a module.
-
-To run executable scripts, from the project root do:
-    python -m src.submodule.script
-
-"""
+"""This makes src a module."""
